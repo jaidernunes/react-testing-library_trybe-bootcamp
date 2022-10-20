42fa5f3f5d9a0449c5b27c008ff580b9a8cb6e29
jaidernunes <jaidernf@gmail.com>
2022-10-20 15:47:34 -0300

    Req 3: Pass

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,34 @@
-// import React from 'react';
-// import userEvent from '@testing-library/user-event';
-// import { screen } from '@testing-library/react';
-// import App from '../App';
-// import renderWithRouter from '../renderWithRouter';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-// test('', () => {});
+describe('Test the FavoritePokemons component ', () => {
+  it('checks if the message "No favorite pokemon found"', () => {
+    renderWithRouter(<App />);
+    const favLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favLink);
 
-// test('verifies the 3 links in the nav', () => {
+    const noFavMsg = 'No favorite pokemon found';
+    const msgEl = screen.getByText(noFavMsg);
 
-// });
+    expect(msgEl).toBeInTheDocument();
+  });
+
+  it('checks if Pikachu is added to the favorites', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+
+    const isFav = screen.getByText('Pokémon favoritado?');
+    userEvent.click(isFav);
+
+    const favLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favLink);
+
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+  });
+});
