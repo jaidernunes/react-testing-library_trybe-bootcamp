7d2c0f706bc566e527c1d4bd0db3e8beca47e505
jaidernunes <jaidernf@gmail.com>
2022-10-21 18:08:08 -0300

    Req 6: Pass!

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,11 +1,45 @@
-// import React from 'react';
-// import userEvent from '@testing-library/user-event';
-// import { screen } from '@testing-library/react';
-// import App from '../App';
-// import renderWithRouter from '../renderWithRouter';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-// describe('App component tests', () => {
-//   it('verifies the 3 links in the nav', () => {
-//   });
-// });
-test('', () => {});
+describe('Pokemon.js component tests:', () => {
+  it('verifies the card generated has the all expected info', () => {
+    renderWithRouter(<App />);
+
+    const pikachuName = screen.getByText(/pikachu/i);
+    const pikachuType = screen.getByTestId('pokemon-type');
+    const pikachuWeight = screen.getByText(/average weight: 6\.0 kg/i);
+    const pikachuImg = screen.getByAltText(/pikachu sprite/i);
+    const pikachuImgUrl = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+
+    expect(pikachuName).toBeInTheDocument();
+    expect(pikachuType.innerHTML).toBe('Electric');
+    expect(pikachuWeight).toBeInTheDocument();
+    expect(pikachuImg).toHaveAttribute('src', pikachuImgUrl);
+  });
+
+  it('verifies if there is a "More details" link and its destination', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
+
+    userEvent.click(moreDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('verifies if the favorited pokemon gets the expected icon on its card', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+
+    const favoriteCheckbox = screen.getByText(/pokémon favoritado\?/i);
+    userEvent.click(favoriteCheckbox);
+
+    const favoriteStar = screen.getByAltText(/pikachu is marked as favorite/i);
+    expect(favoriteStar).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
